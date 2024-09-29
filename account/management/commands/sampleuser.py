8f38c8edfd6b9f182dc3fb785767bd13d4b724ae
ShoAkahori <sho.akahori@outlook.com>
2024-09-29 18:49:25 +0900

    [create]カスタムコマンド(sampleuser)作成

---
+++ account/management/commands/sampleuser.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(username=settings.SAMPLE_NAME).exists():
+            User.objects.create(
+                username=settings.SAMPLE_NAME,
+                email=settings.SAMPLE_EMAIL,
+                password=settings.SAMPLE_PASSWORD,
+                is_staff=True,
+            )
+            print("サンプルユーザー作成")
+        else:
+            print(f"サンプルユーザー[{settings.SAMPLE_NAME}]はすでに存在しています")
