43cdbc3fd30abd4f9744f421d6b62b7011db3389
ShoAkahori <sho.akahori@outlook.com>
2024-09-29 19:39:57 +0900

    [create]admin上にuser関連を表示

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["pk", "username", "email", "icon", "is_superuser"]
+
+admin.site.register(get_user_model(), UserAdmin)
+admin.site.unregister(Group)
