78331b5a6d5efc85acae3d4b52de8d51e3c1250b
ShoAkahori <sho.akahori@outlook.com>
2024-09-30 10:00:03 +0900

    [create]モデル定義, admin上に表示

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Restaurant, BottleManagement, Bottle
 
-# Register your models here.
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display=["pk", "name", "address", "create_user"]
+
+class BottleManagementAdmin(admin.ModelAdmin):
+    list_display=["pk", "management_name", "customer", "restaurant"]
+
+class BottleAdmin(admin.ModelAdmin):
+    list_display=["pk", "management", "liquor_type", "bottle_name", "purchase_date", "storage_period", "is_empty"]
+
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(BottleManagement, BottleManagementAdmin)
+admin.site.register(Bottle, BottleAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+LIQUOR_TYPE = [
+    ("gi", "Gin"),
+    ("vo", "Vodka"),
+    ("te", "Tequila"),
+    ("ru", "Rum"),
+    ("wi", "Whisky"),
+    ("br", "Brandy"),
+    ("sh", "Shochu"),
+    ("ot", "Other")
+]
+
+class Restaurant(models.Model):
+    name = models.CharField(
+        verbose_name="店名",
+        unique=True,
+        max_length=20
+    )
+    address = models.CharField(
+        verbose_name="住所",
+        unique=True,
+        max_length=50
+    )
+    create_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name="ユーザー",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+class BottleManagement(models.Model):
+    customer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name="ユーザー",
+        on_delete=models.CASCADE
+    )
+    restaurant = models.ForeignKey(
+        Restaurant,
+        verbose_name="飲食店",
+        on_delete=models.PROTECT,
+        related_name="bottle_management"
+    )
+    management_name = models.CharField(
+        verbose_name="管理用ID",
+        max_length=10
+    )
+
+    def __str__(self):
+        return f"{self.customer}_{self.management_name}"
+
+class Bottle(models.Model):
+    management = models.ForeignKey(
+        BottleManagement,
+        verbose_name="管理用ID",
+        on_delete=models.CASCADE,
+        related_name="bottle"
+    )
+    liquor_type = models.CharField(
+        verbose_name="アルコール種類",
+        choices=LIQUOR_TYPE,
+        max_length=2
+    )
+    bottle_name = models.CharField(
+        verbose_name="酒名",
+        max_length=20
+    )
+    purchase_date = models.DateField(
+        verbose_name="ボトル購入日",
+        auto_now=True
+    )
+    storage_period = models.DateField(
+        verbose_name="預かり期日",
+        null=True,
+        blank=True
+    )
+    is_empty = models.BooleanField(
+        verbose_name="空ボトルか？",
+        default=False
+    )
+
+    def __str__(self):
+        return self.bottle_name
